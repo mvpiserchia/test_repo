139e5636537508b9a58915b1ffc2448e45d51e27
mvpiserchia <37KBE5WCz!>
2021-01-25 21:08:15 -0500

    added var list.

--- hello_world.py
+++ hello_world.py
@@ -1,9 +1,10 @@
 import pandas as pd
 
 print('Hello, World!')
-
 print('Upload to Git was a success!')
 
 df = pd.DataFrame({'Age': [34,56,18], 'Name': ['Jen', 'Mark', 'Dan']})
-
 print(df)
+
+verbose_var_list = ['One variable in df is named ' + i for i in df.columns]
+print(verbose_var_list)
